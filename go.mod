f0302a9aa46a275d6eed0110524b327e8754838c
Januar Wayong <jwayong@Januars-MacBook-Pro.local>
2023-02-27 10:42:21 +0800

    Updated with some command line update, and change reference to forked repo

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.19
 
 require (
 	github.com/SSSaaS/sssa-golang v0.0.0-20170502204618-d37d7782d752
+	github.com/jwayong/sssa-golang v0.0.0-20170502204618-d37d7782d752
 	github.com/posener/sharedsecret v0.0.0-20200414095807-b90bfadc28af
 )
--- main.go
+++ main.go
@@ -2,25 +2,24 @@ package main
 
 import (
 	"fmt"
-	sssa "github.com/SSSaaS/sssa-golang"
+	sssa "github.com/jwayong/sssa-golang"
 )
 
 func main() {
 	
 	sharesssa,_ := sssa.Create(3, 5, "januarwayong")
-
+	fmt.Println("Encrypting text: januarwayong into 5 parts, minimum 3 parts to recover")
+	fmt.Println("----------------------------------------------------------------------")
 	for _, s := range sharesssa {
-		fmt.Println(s)
-	}
-	fmt.Println("")
-	for _, s := range sharesssa[1:4] {
 		fmt.Println(s)
 	}
 
 	//secretstring,_ := sssa.Combine(sharesssa[1:4])
+	fmt.Println("Decrypting text: using minimum 3 parts")
+	fmt.Println("----------------------------------------------------------------------")
 	secretstring,_ := sssa.Combine([]string{sharesssa[0], sharesssa[2], sharesssa[3]})
 
-
+	fmt.Println("Decrypted text: ")
 	fmt.Println(secretstring)
 
 
